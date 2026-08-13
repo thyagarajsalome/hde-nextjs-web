0eb88eb864d90436def4020450a9d077597e3809
Thyagaraja <your-github-email@example.com>
2026-08-13 18:16:21 +0530

    Add Paint Visualizer CTA button to Hero section

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Link from "next/link";
 import { HeroService, HeroBanner } from "../../services/heroService";
 import { useGSAPHeroParallax } from "../../hooks/useGSAP";
 
