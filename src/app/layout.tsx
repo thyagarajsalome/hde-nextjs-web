362ff709760f9f3f1797dcdf8614c10a11bce0e5
Thyagaraja <your-github-email@example.com>
2026-08-14 11:07:50 +0530

    Fix Server Component build error with inline onLoad event

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en" data-scroll-behavior="smooth">
       <head>
-        <link rel="preload" as="style" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" media="print" onLoad={(e) => { (e.target as HTMLLinkElement).media = "all"; }} />
-        <noscript>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
-        </noscript>
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body className="bg-background text-zinc-900 min-h-screen flex flex-col font-sans">
         <ToastProvider>
